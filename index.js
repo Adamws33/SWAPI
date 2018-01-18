d4f121c5b4f332672e681b70c5a3637efb61fbe6
Adam Smith <adamws33@gmail.com>
2018-01-18 10:41:05 -0500

    grabs all info for people from API

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 $(()=>{
+
+  let loaded = false //this will prevent from loading the info multiple times
+
 $('#infoButton').click(()=>{
+  if(!loaded){
   $.ajax({
     type: 'GET',
     url: 'https://swapi.co/api/people'
     }).done((res)=>{
       let people=res.results;
-      console.log(people);
-      for(person of people){
-        console.log(person.name)
+      for(p of people){
+        $('#tableBody').append(createTableRow(p))
       }
+      loaded = !loaded //this will prevent from loading the info multiple times
     })
+  } else{
+    alert ('the info has already been loaded')
+  }
   })
+
+  function createTableRow(person){
+    let row = $(`<tr></tr>`)
+    let name = $(`<td>${person.name}</td>`)
+    let height = $(`<td>${person.height}</td>`)
+    let birth = $(`<td>${person.birth_year}</td>`)
+
+    row.append(name)
+    row.append(height)
+    row.append(birth)
+    return row
+  }
+
 })
